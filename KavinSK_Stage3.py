0e87c9ec11ff230c3380e2a09955e64339326675
Kavinsk02 <66780940+Kavinsk02@users.noreply.github.com>
2026-02-27 19:59:32 +0530

    Update KavinSK_Stage3.py
    Added input validation for subject marks to ensure they are between 0 and 100.

--- KavinSK_Stage3.py
+++ KavinSK_Stage3.py
@@ -2,7 +2,8 @@ student_name=input("Student Name:")
 sub1=int(input("Subject 1:"))
 sub2=int(input("Subject 2:"))
 sub3=int(input("Subject 3:"))
-
+if (sub1< 0 or sub1 > 100) or (sub2 < 0 or sub2 > 100) or (sub3 < 0 or sub3 > 100):
+    print("Error: Marks must be between 0 and 100 only")
 print(f"Name :{student_name}")
 total=sub1+sub2+sub3
 print(f"Total :{total}/300")
@@ -17,3 +18,4 @@ elif(percentage >=40):
   print("Grade C")
 else:
   print("Grade F")
+
